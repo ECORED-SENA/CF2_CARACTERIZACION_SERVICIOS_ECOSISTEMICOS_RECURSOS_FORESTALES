c09c7937646118b02d1011f57b494fe17faae19b
Leyson Castaño <leysofcp@hotmail.com>
2025-09-26 00:11:32 -0500

    se actualiza CFA

--- src/config/global.js
+++ src/config/global.js
@@ -165,7 +165,7 @@ export default {
       {
         icono: 'fas fa-file-pdf',
         titulo: 'Descargar PDF',
-        download: 'downloads/dist.pdf',
+        download: 'downloads/220201095_CF02_DU.zip',
       },
       {
         icono: 'fas fa-download',
